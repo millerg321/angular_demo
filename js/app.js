0d99c7b98ac2c147e1d606c30bdb66fb53055847
millerg321 <millerg@tcd.ie>
2016-11-16 21:47:30 +0000

    added youtube shizz for the laugh

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', [
   'ngRoute',
-  'artistControllers'
+  'myControllers'
 ]);
 
 myApp.config(['$routeProvider', function($routeProvider) {
@@ -13,7 +13,72 @@ myApp.config(['$routeProvider', function($routeProvider) {
     templateUrl: 'partials/details.html',
     controller: 'DetailsController'
   }).
+  when('/videos', {
+    templateUrl: 'partials/youtube.html',
+    controller: 'youtubeController'
+  }).
   otherwise({
     redirectTo: '/list'
   });
 }]);
+
+myApp.directive('youtube', function($window) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "@"
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+
+        player = new YT.Player(element.children()[0], {
+          playerVars: {
+            autoplay: 0,
+            html5: 1,
+            theme: "light",
+            modesbranding: 0,
+            color: "white",
+            iv_load_policy: 3,
+            showinfo: 1,
+            controls: 1
+          },
+
+          height: scope.height,
+          width: scope.width,
+          videoId: scope.videoid, 
+        });
+      }
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        player.cueVideoById(scope.videoid);
+
+      }); 
+
+      scope.$watch('height + width', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        player.setSize(scope.width, scope.height);
+
+      });
+    }  
+  };
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,13 @@
-var artistControllers = angular.module('artistControllers', ['ngAnimate']);
+var myControllers = angular.module('myControllers', ['ngAnimate']);
 
-artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
+myControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
   });
 }]);
 
-artistControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+myControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.whichItem = $routeParams.itemId;
@@ -25,5 +25,13 @@ artistControllers.controller('DetailsController', ['$scope', '$http','$routePara
     }
 
   });
+}]);
+
+myControllers.controller('YoutubeController', [function($scope) {
+  $scope.yt = {
+      width: 600, 
+      height: 480, 
+      videoid: "QAlSXpl_UtA",
+    };
 }]);
 
